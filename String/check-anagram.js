886c7479283032788a8dafcc8e37e6cef0dea3db
Mohammad Shadman Ansari <ansarishadman@gmail.com>
2021-07-27 16:01:24 +0530

    anagram code added

---
+++ String/check-anagram.js
@@ -0,0 +1,29 @@
+checkAnagram('talent', 'latent');
+
+function checkAnagram(strValue, anagVal) {
+    if (strValue === undefined || anagVal === undefined) {
+        console.log('Please enter a string value!');
+        return;
+    }
+
+    if (strValue.length !== anagVal.length) {
+        console.log('Not anagram!');
+        return;
+    }
+
+    let strValArr = Object.assign([], strValue);
+    let anagValArr = Object.assign([], anagVal);
+
+    strValArr.sort();
+    anagValArr.sort();
+    let j = 0;
+    let isAnagram = true;
+    for (let i = 0; i < strValArr.length && j < anagValArr.length; i++) {
+        if (strValArr[i] !== anagValArr[j]) {
+            isAnagram = false;
+            break;
+        }
+        j++;
+    }
+    isAnagram ? console.log('anagram!') : console.log('ot anagram!');
+}
